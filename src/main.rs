f10c49769c1306c72bb4ca7e2a5f9197d3b46f5b
Adam Sulewski <adam.j.sulewski@gmail.com>
2023-04-04 07:09:32 -0700

    only print hunks that overlap with specs

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,69 @@ use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 
 fn main() {
+    let project_dir = parse();
+    if let Err(e) = run(project_dir) {
+        println!("Error: {}", e);
+    }
+}
+
+fn parse() -> String {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         panic!("Please provide a project directory.");
     }
 
     let project_dir = args[1].clone();
-    if let Err(e) = run(&project_dir) {
-        println!("Error: {}", e);
+    project_dir
+}
+
+#[derive(Debug)]
+struct Specs {
+    start_line: usize,
+    end_line: usize,
+}
+
+impl Specs {
+    fn from_lines(lines: &Vec<String>) -> Specs {
+        // This is an assumption for now.
+        let start_line = 3;
+        let mut end_line = start_line;
+
+        // Find the first empty line. That is the end of the specs section.
+        // This is also an assumption for now.
+        for (i, line) in lines.iter().enumerate() {
+            if line.is_empty() {
+                end_line = i;
+                break;
+            }
+        }
+
+        Specs {
+            start_line,
+            end_line,
+        }
+    }
+
+    fn overlaps(&self, start_line: usize, end_line: usize) -> bool {
+        if start_line > self.end_line {
+            return false;
+        }
+        if end_line < self.start_line {
+            return false;
+        }
+        true
     }
 }
 
-fn run(project_dir: &String) -> Result<(), Box<dyn std::error::Error>> {
-    let project_path = Path::new(project_dir);
+fn run(project_dir: String) -> Result<(), Box<dyn std::error::Error>> {
+    let project_path = Path::new(&project_dir);
     let gemfile_lock = "Gemfile.lock";
     let gemfile_lock_path = Path::new(gemfile_lock);
     let git_dir = ".git";
     let git_path = project_path.join(git_dir);
 
-    let gem_file_lock_lines = read_file_lines(project_path.join(gemfile_lock));
+    let gemfile_lock_lines = read_file_lines(project_path.join(gemfile_lock));
+    let specs = Specs::from_lines(&gemfile_lock_lines);
 
     let repo = Repository::open(git_path).unwrap();
     // Instead of `None` you can also pass a `git2::BlameOptions` object.
@@ -35,16 +79,19 @@ fn run(project_dir: &String) -> Result<(), Box<dyn std::error::Error>> {
         let commit_id = hunk.final_commit_id();
         let seconds = hunk.final_signature().when().seconds();
         let formatted_time = format_seconds(seconds);
-        println!("{} -- {}", commit_id, formatted_time);
 
         let start_line = hunk.final_start_line() - 1 as usize;
         let end_line = start_line + hunk.lines_in_hunk() as usize;
-        println!("Lines {} to {}", start_line + 1, end_line + 1);
-        let hunk_lines = gem_file_lock_lines[start_line..end_line].join("\n");
-        println!("{}", hunk_lines);
 
-        println!();
+        if specs.overlaps(start_line, end_line) {
+            println!("{} -- {}", commit_id, formatted_time);
+            println!("Lines {} to {}", start_line + 1, end_line + 1);
+            let hunk_lines = gemfile_lock_lines[start_line..end_line].join("\n");
+            println!("{}", hunk_lines);
+            println!();
+        }
     }
+
     Ok(())
 }
 
